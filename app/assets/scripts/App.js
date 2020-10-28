7454b8de7346257b779fab6020b72aa1bcc7cb15
Moe <moealamodi@gmail.com>
2020-10-27 21:53:57 -0400

    Completed lazyloading of images.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,9 @@
 import '../styles/styles.css'
+import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
- 
+
 new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
@@ -10,17 +11,15 @@ new MobileMenu();
 let modal
 
 document.querySelectorAll(".open-modal").forEach(el => {
-  el.addEventListener("click", el => {
-    el.preventDefault()
+  el.addEventListener("click", e => {
+    e.preventDefault()
     if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
         modal = new x.default()
         setTimeout(() => modal.openTheModal(), 20)
-      }).catch(() => console.log("there was a problem."))
-
+      }).catch(() => console.log("There was a problem."))
     } else {
       modal.openTheModal()
-
     }
   })
 })
