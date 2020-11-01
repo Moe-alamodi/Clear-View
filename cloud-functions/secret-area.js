ba60f2636f6ed5d66f3639484f50b318492d1245
Moe <moealamodi@gmail.com>
2020-10-31 21:03:24 -0400

    client area section

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,30 +1,26 @@
 exports.handler = function(event, context, callback) {
   const secretContent = `
-  <h3>Welcom to the Secret Area</h3>
+  <h3>Welcome To The Secret Area</h3>
+  <p>Here we can tell you that <strong>6</strong> was afriad of <strong>7</strong> because <strong>7</strong>, <strong>8</strong>, <strong>9</strong>!!</p>
   `
   
   let body
 
   if (event.body) {
     body = JSON.parse(event.body)
-
-  } esle {
+  } else {
     body = {}
-
   }
 
-  if(body.password == "123abc") {
+  if (body.password == "javascript") {
     callback(null, {
       statusCode: 200,
       body: secretContent
     })
-
   } else {
     callback(null, {
       statusCode: 401
-      
     })
-
   }
 
   
