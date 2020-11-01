3e862cb2de90bba17cbbb1a9d63b9fc5c8e34a24
Moe <moealamodi@gmail.com>
2020-10-31 20:33:01 -0400

    Updated could function to check passwoed.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,34 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the super secret area"
-  })
+  const secretContent = `
+  <h3>Welcom to the Secrent Area</h3>
+  <p>Why was <strong>6</strong> afraid of <strong>7</strong>? </p>
+  <p>Because <strong>7</strong>, <strong>8</strong>, <strong>9</strong>!</p>
+  `
+  
+  
+  let body
+
+  if (event.body) {
+    body = JSON.parse(event.body)
+
+  } esle {
+    body = {}
+
+  }
+
+  if(body.password == "123abc") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    })
+
+  } else {
+    callback(null, {
+      statusCode: 401
+      
+    })
+
+  }
+
+  
 }
